c50f8bc9c2342a8a289bf617bdac5fbb3186b0fd
sammyyx <sammyyaoxin@gmail.com>
2016-10-27 20:25:24 +0800

    移除main函数读参数代码,改从已有数据结构启动,增加启动函数start_hostapd_daemon()

---
+++ hostapd/starthostapd.h
@@ -0,0 +1,24 @@
+#include "utils/includes.h"
+#ifndef CONFIG_NATIVE_WINDOWS
+#include <syslog.h>
+#include <grp.h>
+#endif /* CONFIG_NATIVE_WINDOWS */
+
+#include "utils/common.h"
+#include "utils/eloop.h"
+#include "utils/uuid.h"
+#include "crypto/random.h"
+#include "crypto/tls.h"
+#include "common/version.h"
+#include "drivers/driver.h"
+#include "eap_server/eap.h"
+#include "eap_server/tncs.h"
+#include "ap/hostapd.h"
+#include "ap/ap_config.h"
+#include "ap/ap_drv_ops.h"
+#include "fst/fst.h"
+#include "config_file.h"
+#include "eap_register.h"
+#include "ctrl_iface.h"
+
+int start_hostapd_daemon(struct hostapd_simple_config *s_conf);
